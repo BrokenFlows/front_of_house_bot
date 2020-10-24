97f6c1c66bfa9d9978d1e9c527e1f347590ebc62
BrokenFlows <cianfdowd@gmail.com>
2020-10-24 16:50:11 +0100

    fixes f-strings and attempts to get ids from parameteres

--- bot.py
+++ bot.py
@@ -60,9 +60,13 @@ async def puppet(ctx, *, source : str):
 @bot.command()
 async def move(ctx, other_user : str, other_channel : str):
     """Deletes the chosen message and moves it to another channel"""
-    print(f'type(ctx), ctx')
-    print(f'type(other_user), other_user')
-    print(f'type(other_channel), other_channel')
+    print(f'{type(ctx)}, {ctx}')
+    print(f'{type(other_user)}, {other_user}')
+    print(f'{type(other_channel)}, {other_channel}')
+    other_user_id = int(re.search(r'\d*', other_user))
+    other_channel_id = int(re.search(r'\d*', other_channel))
+    print(f'{type(other_user_id)}, {other_user_id}')
+    print(f'{type(other_channel_id)}, {other_channel_id}')
 #    message = ctx.message
 #    if (message.author.id == config.puppet_master):
 #        await message.delete()
